ebb323ade0312cba0d65b31ef6560ba187b0dfac
Will Manson <willrtmanson@gmail.com>
2015-06-08 16:31:33 +0100

    Detect matches and clear matches

--- board.py
+++ board.py
@@ -3,6 +3,7 @@ import random
 class Board:
 	def __init__(self):
 		self.board = [[0 for column in range(6)] for row in range(12)]
+		self.points = 0
 
 	def printBoard(self):
 		for row in self.board:
@@ -23,6 +24,69 @@ class Board:
 			self.board[len(self.board) - 1][j] = random.randint(1, 5)
 			print "Update"
 			self.printBoard()
+
+	def searchForMatchesAndClear(self):
+		matches = []
+		currentTileType = 0
+		currentTileMatchCount = 0
+
+		for j in range(len(self.board[0])):
+			for i in range(len(self.board)):
+				if currentTileType == self.board[i][j]:
+					currentTileMatchCount += 1
+				else:
+					currentTileType = self.board[i][j]
+					currentTileMatchCount = 1
+
+				if currentTileMatchCount == 3:
+					matches.append([i - 2, j])
+					matches.append([i - 1, j])
+					matches.append([i    , j])
+					self.points += 3
+				if (currentTileMatchCount > 3):
+					matches.append([i, j])
+					self.points += 2
+			currentTileType = 0
+			currentTileMatchCount = 0
+
+		currentTileType = 0
+		currentTileMatchCount = 0
+
+		for i in range(len(self.board)):
+			for j in range(len(self.board[i])):
+				if currentTileType == self.board[i][j]:
+					currentTileMatchCount += 1
+				else:
+					currentTileType = self.board[i][j]
+					currentTileMatchCount = 1
+
+				if currentTileMatchCount == 3:
+					matches.append([i, j - 2])
+					matches.append([i, j - 1])
+					matches.append([i, j])
+					self.points += 3
+				if (currentTileMatchCount > 3):
+					matches.append([i, j])
+					self.points += 2
+			currentTileType = 0
+			currentTileMatchCount = 0
+
+		self.clearMatches(matches)
+
+	def clearMatches(self, matches):
+		for match in matches:
+			self.board[match[0]][match[1]] = 0
+
+	def scorePointsAndReplenishBoard(self):
+		while True:
+			self.searchForMatchesAndClear()
+			self.fillBoard()
+			if (self.points == 0):
+				break
+			self.points = 0
+			
 		
 b = Board()
 b.fillBoard()
+b.scorePointsAndReplenishBoard()
+print b.points
